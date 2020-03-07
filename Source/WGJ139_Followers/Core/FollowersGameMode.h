eae18553c09730ebdc47984bae8e8953114b1a96
Master Kenth <masterkenth@gmail.com>
2020-03-07 14:40:58 +0100

    feat: initial project setup

---
+++ Source/WGJ139_Followers/Core/FollowersGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FollowersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WGJ139_FOLLOWERS_API AFollowersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
