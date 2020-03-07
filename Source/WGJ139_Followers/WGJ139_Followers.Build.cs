48da4754fd980b96f46b5f4e8d78fca493cf0904
Master Kenth <masterkenth@gmail.com>
2020-03-07 16:11:17 +0100

    feat: add Paper2D as build dependency

--- Source/WGJ139_Followers/WGJ139_Followers.Build.cs
+++ Source/WGJ139_Followers/WGJ139_Followers.Build.cs
@@ -8,7 +8,7 @@ public class WGJ139_Followers : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
