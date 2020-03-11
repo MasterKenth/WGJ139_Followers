c6d5d34d790e0659a97e0060d9c55792b0206fdb
Master Kenth <masterkenth@gmail.com>
2020-03-11 20:38:45 +0100

    feat: add end result feedback text to game over ui

--- Source/WGJ139_Followers/UI/GameOverWidget.cpp
+++ Source/WGJ139_Followers/UI/GameOverWidget.cpp
@@ -4,9 +4,42 @@
 #include "GameOverWidget.h"
 #include "Components/Button.h"
 #include "Components/TextBlock.h"
+#include "../Core/FollowersGameState.h"
+#include "Engine/World.h"
+#include "Components/HorizontalBox.h"
 
 void UGameOverWidget::NativeConstruct()
 {
+  AFollowersGameState* gameState = GetWorld()->GetGameState<AFollowersGameState>();
+  if(gameState)
+  {
+    int32 followers = gameState->Cults[0].Followers - 1;
+    for(UTextBlock* countText : FollowerCountTexts)
+    {
+      countText->SetText(FText::AsNumber(followers));
+    }
+
+  if(FailedTextContainer && OneFollowerTextContainer && ManyFollowersTextContainer)
+  {
+    FailedTextContainer->SetVisibility(ESlateVisibility::Collapsed);
+    OneFollowerTextContainer->SetVisibility(ESlateVisibility::Collapsed);
+    ManyFollowersTextContainer->SetVisibility(ESlateVisibility::Collapsed);
+    
+    if(followers == 0)
+    {
+      FailedTextContainer->SetVisibility(ESlateVisibility::Visible);
+    }
+    else if(followers == 1)
+    {
+      OneFollowerTextContainer->SetVisibility(ESlateVisibility::Visible);
+    }
+    else
+    {
+      ManyFollowersTextContainer->SetVisibility(ESlateVisibility::Visible);
+    }
+  }
+
+  }
   if(RetryButton)
   {
     // TODO: retry game
--- Source/WGJ139_Followers/UI/GameOverWidget.h
+++ Source/WGJ139_Followers/UI/GameOverWidget.h
@@ -15,9 +15,18 @@ class WGJ139_FOLLOWERS_API UGameOverWidget : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<class UTextBlock*> FollowerCountTexts;
+
 	UPROPERTY(EditAnywhere, meta=(BindWidget))
-	class UTextBlock* FollowerCountText;
+	class UHorizontalBox* FailedTextContainer;
 	
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UHorizontalBox* OneFollowerTextContainer;
+	
+	UPROPERTY(EditAnywhere, meta=(BindWidget))
+	class UHorizontalBox* ManyFollowersTextContainer;
+
 	UPROPERTY(EditAnywhere, meta=(BindWidget))
 	class UButton* RetryButton;
 
