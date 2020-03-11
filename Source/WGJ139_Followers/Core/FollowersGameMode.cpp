f88628ae283c113a67712e518f73cbf0b14148f2
Master Kenth <masterkenth@gmail.com>
2020-03-11 23:55:23 +0100

    fix: set cult + total follower count to correct

--- Source/WGJ139_Followers/Core/FollowersGameMode.cpp
+++ Source/WGJ139_Followers/Core/FollowersGameMode.cpp
@@ -123,8 +123,8 @@ void AFollowersGameMode::SetupCults()
     }
 
     // Generate NPC cults
-    const int32 totalFollowersToGenerate = 3;
-    const int32 numNPCCults = 1;//FMath::RandRange(2, 5);
+    const int32 totalFollowersToGenerate = 99;
+    const int32 numNPCCults = FMath::RandRange(2, 5);
     int32 followersGenerated = 0;
     for(int32 i = 0; i < numNPCCults; i++)
     {
