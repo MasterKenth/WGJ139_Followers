17e00215a3c8bafcf5f41fd2c4ea486cb983a787
Master Kenth <masterkenth@gmail.com>
2020-03-11 19:53:31 +0100

    feat: add SlateCore to build dependencies

--- Source/WGJ139_Followers/WGJ139_Followers.Build.cs
+++ Source/WGJ139_Followers/WGJ139_Followers.Build.cs
@@ -8,7 +8,7 @@ public class WGJ139_Followers : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Paper2D", "AIModule", "SlateCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
